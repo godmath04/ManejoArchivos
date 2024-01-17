684129fc7a41466ba36f6e4e4ebf092ce6879121
Luis Pineda <luis.pineda.revelo@udla.edu.ec>
2024-01-17 16:03:13 -0500

    Se cambio a lectura y ver su error al no existir

--- ejemplo2.c
+++ ejemplo2.c
@@ -3,13 +3,23 @@
 int main(int argc, char const *argv[])
 {
     FILE *archivo; // Variable tipo puntero
-
-    archivo = fopen("Reporte.txt", "w"); // Escribir en el archivo
     char unCaracter;
+    // Forma para validar un archivo
+    if ((archivo = fopen("Reporte.txt", "r")) != NULL)
+    {
+        while (!feof(archivo))
+        {
+            unCaracter = fgetc(archivo);
+            putchar(unCaracter);
+        }
+        fclose(archivo);
 
-    fputs("Esta es una prueba de escritura a un archivo", archivo);
+    }else
+        {
+            printf("Error con el archivo");
+        }
 
-    fclose(archivo);
+        fclose(archivo);
 
-    return 0;
-}
+        return 0;
+    }
